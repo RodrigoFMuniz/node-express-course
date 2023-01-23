746ba3ba986dff48c8088cd85dbeb5f7fe3e62c1
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-01-23 12:45:43 -0300

    Updating material

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial');
+const http = require('http');
+
+const PORT = 5000;
+const server = http.createServer((req, res) => {
+  res.end(`${req.url} , ${req.statusCode}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
