f894f24ef262fed8d78fdaa3d8abeffc67de5765
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-01-24 15:19:52 -0300

    Request object

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,16 @@ const http = require('http');
 
 const PORT = 5000;
 const server = http.createServer((req, res) => {
+  console.log(req.method);
+  console.log(req.url);
+  console.log(req.headers.host);
+  console.log(req.headers.connection);
+  console.log(req.headers['accept-language']);
+  console.log(req.headers.referer);
+  console.log(req.headers['accept-patch']);
   res.writeHead(200, { 'content-type': 'text/html' });
-  res.end('<h1>Home page</h1>');
+  res.write('<h1>Home page</h1>');
+  res.end();
 });
 
 server.listen(PORT, () => {
