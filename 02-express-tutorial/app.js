b3e9d3c156383daf6aa960c5521c647e6a987779
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-01-24 14:55:05 -0300

    Using writeHead() function

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const PORT = 5000;
 const server = http.createServer((req, res) => {
-  res.end('Home page');
+  res.writeHead(200, { 'content-type': 'text/html' });
+  res.end('<h1>Home page</h1>');
 });
 
 server.listen(PORT, () => {
