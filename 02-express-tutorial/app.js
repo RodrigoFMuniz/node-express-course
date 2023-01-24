568dd6a967c99b25955a57290e1a1a21e7eef7a3
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-01-24 15:57:47 -0300

    About page

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,16 +2,28 @@ const http = require('http');
 
 const PORT = 5000;
 const server = http.createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-  console.log(req.headers.host);
-  console.log(req.headers.connection);
-  console.log(req.headers['accept-language']);
-  console.log(req.headers.referer);
-  console.log(req.headers['accept-patch']);
-  res.writeHead(200, { 'content-type': 'text/html' });
-  res.write('<h1>Home page</h1>');
-  res.end();
+  // console.log(req.method);
+  // console.log(req.url);
+  // console.log(req.headers.host);
+  // console.log(req.headers.connection);
+  // console.log(req.headers['accept-language']);
+  // console.log(req.headers.referer);
+  // console.log(req.headers['accept-patch']);
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Home page</h1>');
+    res.end();
+  }
+  if (req.url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>About page</h1>');
+    res.end();
+  }
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>Not Found</h1>');
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
