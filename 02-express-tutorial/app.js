d529f446a5a9081716c4bbaaacdaff3b412c5da0
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-14 20:52:34 -0300

    Use middleware and public folder to execute all static files

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,17 +2,14 @@ const express = require('express');
 
 const app = express();
 
-const path = require('path');
+// const path = require('path');
 
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+// });
 
-app.get('/about', (req, res) => {
-  res.status(200).send('<h1>about page modified</h1');
-});
 
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Resource not found<h1>');
