3f8f08626ce84595773c5ce67bf8a47c9e1ab38d
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-14 20:28:27 -0300

    Express study init

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,58 +1,26 @@
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// const { readFileSync } = require('fs');
-const http = require('http');
-const { retrieveData } = require('./models');
-
-const PORT = 5000;
-const server = http.createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-//   console.log(req.headers.host);
-//   console.log(req.headers.connection);
-//   console.log(req.headers['accept-language']);
-//   console.log(req.headers.referer);
-//   console.log(req.headers['accept-patch']);
-//   const homeIndex = readFileSync('.\\navbar-app\\index.html');
-//   const homeStyles = readFileSync('.\\navbar-app\\styles.css');
-//   const homeLogo = readFileSync('.\\navbar-app\\logo.svg');
-//   const homeBrowserJs = readFileSync('.\\navbar-app\\browser-app.js');
-//   const projects = readFileSync('.\\index.html');
-
-  if (req.url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(retrieveData('.\\navbar-app\\index.html').file);
-    res.end();
-  }
-  else if (req.url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' });
-    res.write(retrieveData('.\\navbar-app\\styles.css').file);
-    res.end();
-  }
-  else if (req.url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' });
-    res.write(retrieveData('.\\navbar-app\\logo.svg').file);
-    res.end();
-  }
-  else if (req.url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    res.write(retrieveData('.\\navbar-app\\browser-app.js').file);
-    res.end();
-  }
-  else if (req.url === '/projects.html') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(retrieveData('.\\index.html').file);
-    res.end();
-  }
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' });
-    res.write('<h1>Not Found</h1>');
-    res.end();
-  }
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>hello world!!!</h1');
+});
+app.get('/about', (req, res) => {
+  res.status(200).send('<h1>about page modified</h1');
 });
 
-server.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+app.all('*', (req, res)=>{
+  res.status(404).send('<h1>Resource not found<h1>')
+})
+
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
 });
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
--- 02-express-tutorial/models.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-function retrieveData(path, contentType) {
-  const file = fs.readFileSync(path);
-  return { file, contentType };
-}
-
-module.exports = { retrieveData };
