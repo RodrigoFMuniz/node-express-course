3afc1cb13491eb700f9808283f1ae48c5725cd7d
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-14 20:40:05 -0300

    apply middleware express.static to the study

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,16 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>hello world!!!</h1');
+  res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
+
 app.get('/about', (req, res) => {
   res.status(200).send('<h1>about page modified</h1');
 });
 
-app.all('*', (req, res)=>{
-  res.status(404).send('<h1>Resource not found<h1>')
-})
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource not found<h1>');
+});
 
 app.listen(5000, () => {
   console.log('Listening on port 5000');
--- 02-express-tutorial/public/browser-app.js
+++ 02-express-tutorial/public/browser-app.js
@@ -1,9 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
-console.log(navToggle)
 navToggle.addEventListener('click', (ev) => {
-  console.log(ev.currentTarget)
   ev.currentTarget.classList.toggle('rodar');
   links.classList.toggle('show-links');
 });
