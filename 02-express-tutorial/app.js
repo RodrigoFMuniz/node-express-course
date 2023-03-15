bc6c80efcaf09c65f27003a00193863859d616ac
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-14 22:00:05 -0300

    URL params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,35 @@
 const express = require('express');
 
 const app = express();
-const {products} = require('./data.js')
+const { products } = require('./data');
 // const path = require('path');
 
 // app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   // res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-  res.status(200).json(products);
+  res.status(200).send('<h1>Home</h1><br><a href="/api/products">Products</a><br><a href="/api/products/filtered">Products Filtered</a>');
 });
 
+app.get('/api/products', (req, res) => {
+  res.status(200).json(products);
+});
+app.get('/api/products/filtered', (req, res) => {
+  const { newProducts } = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.status(200).json(newProducts);
+});
+app.get('/api/products/url/:productId', (req, res) => {
+  const { productId } = req.params;
+  const newProduct = products.find((product) => product.id === Number(productId));
+
+  if (!newProduct) {
+    res.status(404).send('Product doesn\'t exists');
+  }
+  res.status(200).json(newProduct);
+});
 
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Resource not found<h1>');
