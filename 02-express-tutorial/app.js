0cfbe8b7219f090d0d8bbf5ee6ecf6a6d46192c1
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-18 16:15:19 -0300

    POST method using express.urlencoded middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,21 +4,21 @@ const app = express();
 
 const { people } = require('./data');
 
-const func = (req, res, next) => {
-  console.log('Hello')
-  req.value = { name: 'World' };
-  next();
-}
+// static asets
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/:uid', func, (req, res) => {
-  if (req.params.uid === '2') return res.status(200).json({ success: true, data: people });
-  return res.status(404).send('Unauthorized');
-});
-app.get('/', func, (req, res) => {
-  if (req.query.uid === '2') return res.status(200).json({ success: true, data: people });
-  return res.status(404).send('Unauthorized');
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
+app.post('/login', (req, res) => {
+  if (!req.body.name) {
+    return res.status(404).send("Data wasn't provided");
+  }
+  return res.status(200).send(`Welcome ${req.body.name}`);
+});
 app.listen(5000, () => {
   console.log('Listening on port 5000');
 });
