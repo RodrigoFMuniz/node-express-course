38d59a7af17555282f894398c86c8370c5ed4d98
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-18 14:48:53 -0300

    GET methods using quer and params. All of them with middlewares

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,31 +2,21 @@ const express = require('express');
 
 const app = express();
 
-// const morgan = require('morgan');
+const { people } = require('./data');
 
-const { logger } = require('./logger');
+const func = (req, res, next) => {
+  console.log('Hello')
+  req.value = { name: 'World' };
+  next();
+}
 
-const authorize = require('./authorize');
-
-// app.use(morgan('tiny'));
-app.use('/about', [logger, authorize]);
-
-app.get('/', (req, res) => {
-  res.send('Home');
+app.get('/:uid', func, (req, res) => {
+  if (req.params.uid === '2') return res.status(200).json({ success: true, data: people });
+  return res.status(404).send('Unauthorized');
 });
-app.get('/about', (req, res) => {
-  console.log(req.user);
-  console.log(req.values);
-  // res.send(`About logged ${req.user.name}`);
-  res.send(`About logged Rodrigo ${req.values.time}`);
-});
-app.get('/product', (req, res) => {
-  console.log(req.values);
-  console.log(req.user);
-  res.send('Product');
-});
-app.get('/items', (req, res) => {
-  res.send('items');
+app.get('/', func, (req, res) => {
+  if (req.query.uid === '2') return res.status(200).json({ success: true, data: people });
+  return res.status(404).send('Unauthorized');
 });
 
 app.listen(5000, () => {
