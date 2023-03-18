8b50946630dc465527d0163ede673ad841751d82
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-18 01:19:41 -0300

    applying morgan library to understand how to use external middleware libraries

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,18 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const morgan = require('morgan');
+
 const { logger } = require('./logger');
 
-const authorize = require('./authorize');
+// const authorize = require('./authorize');
 
-app.use('/about', [logger, authorize]);
+app.use(morgan('tiny'));
+// app.use('/about', [logger, authorize]);
 
 app.get('/', (req, res) => {
   res.send('Home');
 });
 app.get('/about', (req, res) => {
   console.log(req.user);
-  res.send(`About logged ${req.user.name}`);
+  // res.send(`About logged ${req.user.name}`);
+  res.send('About logged Rodrigo');
 });
 app.get('/product', (req, res) => {
   res.send('Product');
