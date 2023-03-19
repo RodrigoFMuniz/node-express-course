e3158b87ce3aa5854bb6fe1755633f0b264d442d
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-03-18 22:29:00 -0300

    Post using javascript. Middleware use.app(express.json())

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -9,6 +9,9 @@ app.use(express.static('./methods-public'));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 
+// parse json
+app.use(express.json());
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
@@ -19,6 +22,13 @@ app.post('/login', (req, res) => {
   }
   return res.status(200).send(`Welcome ${req.body.name}`);
 });
+app.post('/api/people', (req, res) => {
+  console.log(req.body)
+  if (!req.body.name) return res.status(400).json({ success: false, msg: 'please provide name value' });
+  return res.status(201).json({ success: true, person: req.body.name });
+});
+
+
 app.listen(5000, () => {
   console.log('Listening on port 5000');
 });
