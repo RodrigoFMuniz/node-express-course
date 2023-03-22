1cebd984d9007eeb0ceffffb144588cc0ac24c7e
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-03-22 13:22:52 -0300

    Add auth route

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const people = require('./routes/people');
 
+const auth = require('./routes/auth');
+
 // static asets
 app.use(express.static('./methods-public'));
 // parse form data
@@ -14,14 +16,8 @@ app.use(express.json());
 
 app.use('/api/people', people.router);
 
-app.post('/login', (req, res) => {
-  if (!req.body.name) {
-    return res.status(404).send("Data wasn't provided");
-  }
-  return res.status(200).send(`<h1>Welcome ${req.body.name}</h1>`);
-});
+app.use('/login', auth);
 
 app.listen(5000, () => {
   console.log('Listening on port 5000');
 });
-
