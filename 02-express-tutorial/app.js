221949cfef1a087097d0c048fbc5294479862d1f
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2023-09-23 19:12:53 -0300

    URL params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -27,6 +27,26 @@ app.get('/api/products', (req, res)=>{
   return res.json(newProducts);
 })
 
+// Single URL params
+app.get('/api/products/:productID', (req, res) => {
+  console.log(req);
+  console.log(req.params);
+  const singleProduct = products.find((product) => product.id === Number(req.params.productID));
+  if (singleProduct) res.json(singleProduct);
+  // else res.status(404).json({ Details: `bad request for ${req.params.productID}` });
+  else res.status(404).sendFile(path.resolve(__dirname, './assets/404.html'));
+})
+
+// multiple URL params
+app.get('/api/products/:productID/v1/:reviewID', (req, res) => {
+  console.log(req);
+  console.log(req.params);
+  const singleProduct = products.find((product) => (product.id === Number(req.params.productID) && (req.params.reviewID === "ola") ));
+  if (singleProduct) res.json(singleProduct);
+  // else res.status(404).json({ Details: `bad request for ${req.params.productID}` });
+  else res.status(404).sendFile(path.resolve(__dirname, './assets/404.html'));
+})
+
 // Using SSR approach
 
 //First method, just leaving the index.html file inside of express.static folder.
